2e03b2eb5acc40a369662e0dc8bbb618f0256a21
Jeremy Mezon <j.mezon@caribemedia.com.do>
2022-01-19 17:57:33 -0400

    Inicialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="sec-heading d-flex d-center">
+    <div class="container text-center">
+      <h1>Learning <span>Vue JS</span></h1>
+    </div>
+  </section>
+  <section class="sec-cards">
+    <div class="container">
+      <h2 class="title-container">Game List</h2>
+      <div class="grid-cards">
+        <Card 
+        title="Red Dead Redemption"
+        imgSrc="https://missingnumber.com.mx/wp-content/uploads/2018/05/Portada-Red-Dead.jpg"
+        imgAlt="Red Dead"
+        text="Prueba"
+        />
+        <Card></Card>
+        <Card></Card>
+      </div>
+      
+    </div>
+    
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from '@/components/Card'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Card },
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
+
+  body{
+    font-family: 'Lato', sans-serif;
+  }
+
+  .sec-heading{
+    width: 100%;
+    min-height: 400px;
+    position: relative;
+    background-color: #35495E;
+    h1{
+      
+      font-weight: 300;
+      color: white;
+      font-size: 64px;
+      span{
+        font-weight: 600;
+        text-transform: uppercase;
+      }
+    }
+  }
+
+  .title-container{
+    font-size: 46px;
+    font-weight: 800;
+    color: #35495E;
+    margin-bottom: 70px;
+  }
+
+  .sec-cards{
+    padding: 100px 0;
+  }
+
+  .grid-cards{
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    grid-gap: 20px;
+  }
+
+  .d-center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="card-component">
+    <div class="card-component-image">
+      <img :src="imgSrc" class="img-fluid" :alt="imgAlt" />
+    </div>
+    <div class="card-component-content">
+      <h4>{{ titleCard }}</h4>
+      <p>{{ textCard }}</p>
+      <ul>
+          <li v-for="genere in generes" :key="genere">
+              {{genere}}
+          </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+  //   data() {
+  //     return {
+  //       titleCard: "Titulo de Tarjeta",
+  //     textCard: "Un simple texto de prueba",
+  //     imgSrc: "https://missingnumber.com.mx/wp-content/uploads/2018/05/Portada-Red-Dead.jpg",
+  //     imgAlt: "Red Dead Redemptions 2"
+  //     };
+  //   },
+
+  setup() {
+    let titleCard = "Titulo de Tarjeta";
+    let textCard = "Un simple texto de prueba";
+    let imgSrc = "https://missingnumber.com.mx/wp-content/uploads/2018/05/Portada-Red-Dead.jpg";
+    let imgAlt = "Red Dead Redemptions 2";
+    let generes = ["Action","Adventure","Sandbox","Violence"]
+    return { titleCard,textCard,imgSrc,imgAlt,generes };
+  }
+};
+</script>
+
+<style lang="scss">
+.card-component {
+  border-radius: 12px;
+  background-color: #f8f8f8;
+  margin: 10px;
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  .card-component-image {
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+    max-width: 200px;
+    img {
+      object-fit: cover;
+      object-position: center;
+      height: 100%;
+    width: 100%;
+    }
+  }
+  .card-component-content {
+    padding: 10px 20px 0px 20px;
+    h4 {
+      font-size: 21px;
+      font-weight: 600;
+      color: #3fb983;
+    }
+    p {
+      font-size: 14px;
+    }
+    ul li{
+        font-size: 10px;
+    }
+  }
+}
+</style>
